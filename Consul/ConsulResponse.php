143f2df6f0f603a4d64d27eb129ef4d0c408d47a
Jordie Bodlay <jordie.bodlay@caradvice.com.au>
2016-10-06 11:44:56 +1100

    Add some destructuring for the returned key/values so they can be retrieved easily

--- Consul/ConsulResponse.php
+++ Consul/ConsulResponse.php
@@ -6,16 +6,49 @@ class ConsulResponse
 {
     private $headers;
     private $body;
+    private $values;
 
     public function __construct($headers, $body)
     {
         $this->headers = $headers;
         $this->body = $body;
+        $this->deconstructBody();
+    }
+
+    private function deconstructBody() {
+      $body = $this->json();
+      if(!is_array($body)) {
+        return;
+      }
+
+      foreach($body as $kv) {
+        $key = end(explode('/',$kv['Key']));
+        $values[$key] = base64_decode($kv['Value']);
+      }
+
+      $this->values = $values;
+
     }
 
     public function getHeaders()
     {
         return $this->headers;
+    }
+
+    public function __toString() {
+      $out = [];
+      foreach($this->values as $k => $v) {
+        $out[] =  "$k,$v";
+      }
+      return implode(';', $out);
+    }
+
+    public function getValues() {
+      return $this->values;
+    }
+
+    public function getVals() {
+      return $this->getValues();
     }
 
     public function getBody()
@@ -27,4 +60,5 @@ class ConsulResponse
     {
         return json_decode($this->body, true);
     }
+
 }
